2f8bc0ec45cffb7f817960bcf8fc099318738a76
Queen-25 <sdiorsows@gmail.com>
2026-05-17 12:18:52 +0000

    Add contact, registration, and profile pages with styling and JavaScript functionality
    - Created `contacte.html` for user message submission with a form.
    - Developed `inscription.html` for user registration with input fields.
    - Added `profil.html` to display user profile information.
    - Introduced `designe.css` for styling across all pages.
    - Included `script.js` for basic JavaScript interactions and alerts.
    - Added a logo image `logo ucao.jpg` for branding.

---
+++ td de html et css/script.js
@@ -0,0 +1,7 @@
+alert('welcom')
+var reponse = prompt('quelle est ta couleur favorite?')
+alert("votre reponse est : " + reponse)
+var confirmation = confirm(" voulez vous continuer la navigation sur le site?")
+alert('vous avez repondu : ' + confirmation)
+var couleur = ['vert', 'jaune', 'rouge']
+alert(couleur[0] + ' ' + couleur[1] + ' ' + couleur[2])
